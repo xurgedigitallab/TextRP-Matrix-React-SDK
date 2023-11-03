262a5a359b6d5ac37a0bbefd57503629f4c6b46d
FCI-1589 <fci-1589@sn-MacBook-Pro.local>
2023-11-03 17:19:16 +0530

    microtxn integrated

--- src/components/structures/UserView.tsx
+++ src/components/structures/UserView.tsx
@@ -19,7 +19,8 @@ import React from "react";
 import { MatrixEvent } from "matrix-js-sdk/src/models/event";
 import { RoomMember } from "matrix-js-sdk/src/models/room-member";
 import { MatrixClient } from "matrix-js-sdk/src/matrix";
-
+import SdkConfig from "../../SdkConfig";
+import axios from "axios";
 import { MatrixClientPeg } from "../../MatrixClientPeg";
 import Modal from "../../Modal";
 import { _t } from "../../languageHandler";
@@ -48,11 +49,58 @@ export default class UserView extends React.Component<IProps, IState> {
             loading: true,
         };
     }
+    public extractWalletAddress = (inputString: string) => {
+        // Define a regular expression pattern to match XRPL addresses
+        const addressRegex = /@([a-zA-Z0-9]{25,34})/;
+
+        // Use the RegExp.exec method to find the address in the input string
+        const match = addressRegex.exec(inputString);
+
+        // Check if a match was found and extract the address
+        if (match && match[1]) {
+            return match[1];
+        }
+        // Return null if no address was found
+        return null;
+    };
+    public generatePaymentLink = async () => {
+        //         const notificationMessage = isKYCVerified(senderId) ?
+        //   '🔒 Secure Message Alert! 🌟 You have a verified message waiting for you on TextRP from a KYC-verified sender. Rest assured, your security is our top priority. View your message safely by logging in at app.textrp.io.' :
+        //   '🔒 Secure Message Alert! 🚀 You have a new message on TextRP. While the sender hasn't been KYC-verified, we ensure your login and viewing experience remains safe. Access your message at app.textrp.io.';
+        try {
+            const res = await axios.post(
+                `${SdkConfig.get("backend_url")}/accounts/${this.extractWalletAddress(this.props.userId)}/payments`,
+                {
+                    message:
+                        "🔒 Secure Message Alert! 🌟 You have a verified message waiting for you on TextRP from a KYC-verified sender. Rest assured, your security is our top priority. View your message safely by logging in at app.textrp.io.",
+                    amount: "0.01",
+                },
+                {
+                    headers: {
+                        "Content-Type": "application/json", // Set the content type to JSON
+                    },
+                },
+            );
+            console.log("GGGGGGG", res);
+            
+            // window.open(res?.data?.data?.next?.always, '_blank')
+        } catch (e) {
+            console.error("ERROR handleBuyCredits", e);
+        }
+    };
 
     public componentDidMount(): void {
         if (this.props.userId) {
             this.loadProfileInfo();
         }
+        this.generatePaymentLink();
+        const cli = MatrixClientPeg.get();
+        cli.createRoom({
+            visibility: "private",
+            invite: ["@rU5kJVrcojGMZeUCG2xnhZcea9rDCoYMsq:synapse.textrp.io", this.props.userId],
+        }).then((e)=>console.log("YYYOYOYOYOYOYOOYOY7", e));
+        
+
     }
 
     public componentDidUpdate(prevProps: IProps): void {
